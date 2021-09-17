937d976312949f0285d4ac013f579484900e171f
= <=>
2021-09-17 18:43:49 +0700

    https://github.com/ctrtinh/tes3.git

--- vd/Controllers/HomeController.cs
+++ vd/Controllers/HomeController.cs
@@ -27,5 +27,18 @@ namespace vd.Controllers
 
             return View();
         }
+        public ActionResult About1()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact`()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
